3832e6cca73553c6f03af34e7c9ca99eacea5dc3
Filip Jukic <filip@jukic.me>
2014-06-16 12:42:15 +0200

    moved the backend to a separate repo

--- backend/ips/stats/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CPUInfo, NetworkInfo, DOMElementCount
-
-admin.site.register(CPUInfo)
-admin.site.register(NetworkInfo)
-admin.site.register(DOMElementCount)
